0530e56bbd85358f5fd08bd8d378969f99848b41
Jose Gonzalez <jose.gonzalezz0808@gmail.com>
2019-03-08 02:27:20 +0000

    giphy

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var giphy = "https://media3.giphy.com/media/Y4pAQv58ETJgRwoLxj/200_s.gif?cid=e1bb72ff5c817f5d33796c6e6bdfdfc6";
+
 $("#search-button").click(function(){
-  
-  
-  
+  $.ajax({
+        url: giphy,
+        method: "GET",
+        success: function(response) {
+        alert(response);
+        }
+  });
 });
 
