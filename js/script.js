73eeecfe3651b74257cd4e94cea5ddad11cd3996
Jose Gonzalez <jose.gonzalezz0808@gmail.com>
2019-03-15 01:24:56 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@
 $("#search-button").click(function(){
 var input = $("#search-term").val();
 var giphy = "https://api.giphy.com/v1/gifs/search?q=" + input + "&rating=pg&api_key=dc6zaTOxFJmzC";
-
+var random = Math.floor(Math.random()*25);
   $.ajax({
         url: giphy,
         method: "GET",
         success: function(response) {
-        var giph = response.data[0].images.original.url;
+        var giph = response.data[random].images.original.url;
         $(".text-center").html('<img src="' + giph + '">');
         }
+        
   });
 });
 
